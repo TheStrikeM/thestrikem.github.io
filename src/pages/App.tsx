2ce753434d2c7ed62c6c55f9f2e4433bbd7a450a
TheStrikeM <dinkodanil@gmail.com>
2021-06-23 23:11:34 +0500

    Add default structure

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+type AppProps = {
+    name?: string
+};
+export const App: React.FC<AppProps>  = (props: AppProps) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
